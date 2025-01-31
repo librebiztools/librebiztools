5276b0e694ef6b44e0cdf19637872f229e60c334
Richard Leek <richard@richardleek.com>
2025-01-31 01:00:59 +0100

    Don't throw errors in config

--- app/api.server/config.ts
+++ app/api.server/config.ts
@@ -7,22 +7,10 @@ interface Config {
   SESSION_TIMEOUT_MINUTES: number;
 }
 
-if (!process.env.POSTGRES_USER) {
-  throw new Error('Missing POSTGRES_USER. Was it specified in .env?');
-}
-
-if (!process.env.POSTGRES_PASSWORD) {
-  throw new Error('Missing POSTGRES_PASSWORD. Was it specified in .env?');
-}
-
-if (!process.env.POSTGRES_DB) {
-  throw new Error('Missing POSTGRES_DB. Was it specified in .env?');
-}
-
 const config = {
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  POSTGRES_DB: process.env.POSTGRES_DB,
+  POSTGRES_USER: process.env.POSTGRES_USER || 'librebiztools',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'password',
+  POSTGRES_DB: process.env.POSTGRES_DB || 'librebiztools',
   SESSION_TIMEOUT_MINUTES: 20,
 } as Config;
 
