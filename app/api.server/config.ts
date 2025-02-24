b6797d44127f38518014cc2d79725557ab8270cc
Richard Leek <richard@richardleek.com>
2025-02-24 21:53:44 +0100

    Add postgres host env variable

--- app/api.server/config.ts
+++ app/api.server/config.ts
@@ -4,6 +4,7 @@ interface Config {
   POSTGRES_USER: string;
   POSTGRES_PASSWORD: string;
   POSTGRES_DB: string;
+  POSTGRES_HOST: string;
   SESSION_TIMEOUT_MINUTES: number;
   BASE_URL: string;
 }
@@ -12,6 +13,7 @@ const config = {
   POSTGRES_USER: process.env.POSTGRES_USER || 'librebiztools',
   POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'password',
   POSTGRES_DB: process.env.POSTGRES_DB || 'librebiztools',
+  POSTGRES_HOST: process.env.POSTGRES_HOST || 'localhost',
   SESSION_TIMEOUT_MINUTES: 20,
   BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
 } as Config;
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST } =
+  process.env;
 
 export default defineConfig({
   out: './drizzle',
   schema: './app/api.server/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost/${POSTGRES_DB}`,
+    url: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_DB}`,
   },
 });
