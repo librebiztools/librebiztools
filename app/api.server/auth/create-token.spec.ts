d61aeefae2d720ec7f221822654fd52e99b9cdfc
Richard Leek <richard@richardleek.com>
2025-03-02 14:25:51 +0100

    Create token test

---
+++ app/api.server/auth/create-token.spec.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+import { eq } from 'drizzle-orm';
+import { expect, test } from 'vitest';
+import config from '../config';
+import { db } from '../db';
+import { tokens } from '../db/schema';
+import { getUserByEmail } from '../users';
+import { signup } from './signup';
+
+test('Creates valid token', async () => {
+  const email = faker.internet.email();
+
+  await signup({
+    name: faker.person.firstName(),
+    email,
+    password: 'password',
+    confirmPassword: 'password',
+    workspaceName: faker.company.name(),
+  });
+
+  const user = await getUserByEmail(email);
+
+  if (!user) {
+    throw new Error('Expected user to be defined');
+  }
+
+  const token = await db.query.tokens.findFirst({
+    where: eq(tokens.userId, user.id),
+  });
+
+  expect(token?.maxAge).toBe(config.SESSION_TIMEOUT_MINUTES * 60);
+});
